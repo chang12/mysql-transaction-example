fe263ea82d7294970ec20db7f0486c7842f2f6ad
Changhyun Lee <chang12@snu.ac.kr>
2019-04-08 02:01:14 +0900

    Item 에 @DynamicUpdate 추가

--- src/main/kotlin/kr/fakenerd/Item.kt
+++ src/main/kotlin/kr/fakenerd/Item.kt
@@ -1,9 +1,11 @@
 package kr.fakenerd
 
+import org.hibernate.annotations.DynamicUpdate
 import javax.persistence.Entity
 import javax.persistence.Id
 
 @Entity
+@DynamicUpdate
 data class Item (
     @Id
     var id: Int? = null,
